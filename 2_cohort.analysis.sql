77b254fef21c2b39638a46af025ac161a73dacae
Priyansh5733 <priyanshgautam110845@gmail.com>
2026-02-04 17:31:20 +0530

    Add files via upload

---
+++ 2_cohort.analysis.sql
@@ -0,0 +1,7 @@
+SELECT cohort_year,
+count(DISTINCT customerkey) AS total_customers,
+sum(net_revenue) AS total_net_revenue,
+sum(net_revenue)/count(DISTINCT customerkey) AS customer_revenue
+FROM cohort_analysis ca 
+WHERE orderdate=ca.first_purchase_date 
+GROUP BY cohort_year 
